385ed01226b1fe126e295715f7af796367052dd8
NikolinaA123 <nikolina.aleksic.2000@gmail.com>
2022-05-26 20:28:35 +0200

    Corrected Repository

--- Repository.py
+++ Repository.py
@@ -1,19 +1,14 @@
 from email import message
 import MySQLdb
 class Repository:
-
     def __init__(self, connection):
-
         self.connection = connection.CreateConnection()
 
-
-
     def doQuery(self,query):
-
         cur = self.connection.cursor()
-
         try:
-            list=cur.execute(query)
+            cur.execute(query)
+            list=cur.fetchall()
             return [2000, list]
         except (MySQLdb.Error,MySQLdb.Warning) as e:
             return [3000, e.args[1]]
