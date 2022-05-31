d5003a3e00f0bdd8936c66c0df8e2dcda00c9753
Jelena Zivanovic <zivanovicjelena000@gmail.com>
2022-05-31 16:38:30 +0200

    Konvertovanje iz Xml u Json format

--- JsonXmlAdapter.py
+++ JsonXmlAdapter.py
@@ -1,5 +1,6 @@
 import json as j
 import xml.etree.cElementTree as e
+from bs4 import BeautifulSoup
 class JsonXmlAdapter:
     def JsonToXml(self, filename):
         with open(filename) as json_format_file:
@@ -12,4 +13,32 @@ class JsonXmlAdapter:
             a = e.ElementTree(r)
             a.write("json_to_xml.xml")
             return "json_to_xml.xml"
+    
+    def XmlToJson(self, fileName):
+        with open(fileName, 'r') as f:
+            data = f.read()
+        bs_data = BeautifulSoup(data, 'xml')
+        statusTag = bs_data.find("status")  #izvadi mi ceo tag
+        status = statusTag.text #iyvadi mi tekst iz taga
+        status_codeTag = bs_data.find("status_code")  #izvadi mi ceo tag
+        status_code=status_codeTag.text
+        payloadTag=bs_data.find("payload")
+        payload=payloadTag.text
+
+        if(len(payload) == 0):
+            payload="Operation successfull"
+
+        json_data={
+            "status":status,
+            "status_code" : status_code,
+            "payload" : payload
+        }
+
+        json_object=j.dumps(json_data, indent=3)
+
+        outputFile="xml_to_json.json";
+        with open(outputFile, "w") as file:
+            file.write(json_object)
+        
+        return outputFile
 
