ee216c53bfe1ea78b1edf382c52007520e543bf1
NikolinaA123 <nikolina.aleksic.2000@gmail.com>
2022-06-10 13:14:10 +0200

    Repository

--- Repository.py
+++ Repository.py
@@ -1,16 +1,60 @@
-from email import message
 import MySQLdb
+import socket
+from ConnectSQL import ConnectToMySQL
+import json
+
+def openConnection(repository):
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    serverSocket.bind(('localhost', 10004))
+    serverSocket.listen(1)
+    while(1):
+        clientConnection, addr = serverSocket.accept();
+        data=clientConnection.recv(1024);
+        if not data:
+            break
+
+        print("INPUT :", data.decode("utf-8"))
+        response=repository.doQuery(data.decode("utf-8"))
+
+        json_string = json.dumps(response)
+
+        clientConnection.sendall(bytes(json_string,encoding="utf-8"))
+        clientConnection.close()
+    serverSocket.close()
+
 class Repository:
     def __init__(self, connection):
         self.connection = connection.CreateConnection()
 
     def doQuery(self,query):
-        cursor = self.connection.cursor()
+        cursor = self.connection.cursor(MySQLdb.cursors.DictCursor)
+        
         try:
             cursor.execute(query)
             data=cursor.fetchall()
-            return [2000, data]
+
+            dict = {
+                "status_code":2000,
+                "status":"SUCCESS",
+                "payload":data,
+            }
+            return dict
         except (MySQLdb.Error,MySQLdb.Warning) as warning:
+            statusCode = 3000
+            status = "REJECTED"
             if(warning.args[0] == 1064):
-                return [5000, warning.args[1]]
-            return [3000, warning.args[1]]
+                statusCode = 5000
+                status = "BAD_FORMAT"
+            
+            dict = {
+                "status_code":statusCode,
+                "status":status,
+                "payload":warning.args[1],
+            }
+            return dict
+
+connection=ConnectToMySQL()
+repository=Repository(connection)
+
+openConnection(repository)
