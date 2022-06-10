f4cffccf3c7f5afd7d83d36dce98964036f4ba96
NikolinaA123 <nikolina.aleksic.2000@gmail.com>
2022-06-10 22:01:15 +0200

    Test metode Insert u XMLDataBaseAdapter-u

--- test_XMLDataBaseAdapter.py
+++ test_XMLDataBaseAdapter.py
@@ -14,6 +14,12 @@ class TestXMLDataBaseAdapter(unittest.TestCase):
         ret_value=xmlDB.DeleteMethod(xml_string)
         correct="DELETE FROM radnik WHERE ime='Ana' and jmbg=12 ; commit;"
         self.assertEqual(ret_value, correct)
+    def test_Insert(self):
+        xml = "<data><verb>POST</verb><noun>radnik</noun><query>jmbg = 44; ime  = Ana; opis = nesto; id_vrsta = 111</query></data>"
+        xmlDB = XMLDateBaseAdapter()
+        retValue = xmlDB.InsertMethod(xml)
+        sql1 = "INSERT INTO radnik VALUES (  44,  Ana,  nesto,  111); commit;"
+        self.assertEqual(retValue, sql1 )
 
 
 
