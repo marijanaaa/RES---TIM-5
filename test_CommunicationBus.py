bb0ecfa264fba43780b077dddaea74a7a17dda85
Jelena Zivanovic <zivanovicjelena000@gmail.com>
2022-06-11 02:12:33 +0200

    Testovi funkcija logike CommunicationBus-a

--- test_CommunicationBus.py
+++ test_CommunicationBus.py
@@ -1,6 +1,8 @@
 import unittest
 import json
-from CommunicationBus import badRequest, returnIsValid
+from CommunicationBus import badRequest, returnIsValid, CommunicationBus
+from JsonXmlAdapter import JsonXmlAdapter
+from unittest.mock import MagicMock, patch
 
 class TestCommunicationBus(unittest.TestCase):
     def test_badRequest(self):
@@ -36,7 +38,16 @@ class TestCommunicationBus(unittest.TestCase):
         expected_response=False
         response=returnIsValid(dict_data)
         self.assertEquals(response, expected_response)
-
+    @patch("JsonXmlAdapter.JsonXmlAdapter")
+    def test_executeRequest(self, mock_JsonXMLAdapterConstructor):
+        mock_json=mock_JsonXMLAdapterConstructor.return_value
+        mock_json.JsonToXml.return_value="<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<data><verb>GET</verb><noun>radnik</noun><query>ime='Ana'</query></data>"
+        comm=CommunicationBus()
+        jsonString = "{\"verb\": \"GET\",\"noun\": \"radnik\", \"query\":\"ime='Ana'\"}"
+        expected_response = json.dumps({"status_code": "2000", "status": "SUCCESS", "payload": "(1003, 'Ana', 'Medicinski tehnicar  u odeljenju za ginekologiju', 2)"})
+        comm.connectToAdapter=MagicMock(return_value = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<data><status_code>2000</status_code><status>SUCCESS</status><payload>(1003, 'Ana', 'Medicinski tehnicar  u odeljenju za ginekologiju', 2)</payload></data>")
+        self.assertEqual(comm.ExecuteRequest(jsonString), expected_response)
+        
 
 if __name__ == '__main__':
     unittest.main()
