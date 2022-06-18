5503d2208fe9e71f78bf1c64c40f4c0bdf8a4151
NikolinaA123 <nikolina.aleksic.2000@gmail.com>
2022-06-18 20:07:38 +0200

    Izmena naziva promenljivih i naziva funkcija

--- CommunicationBus.py
+++ CommunicationBus.py
@@ -8,45 +8,45 @@ verbs = [
     "PATCH",
     "DELETE",
 ]
-def openConnection(communicationBus):
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    serverSocket.bind(('localhost', 10005))
-    serverSocket.listen(1)
+def open_connection(communication_bus):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(('localhost', 10005))
+    server_socket.listen(1)
     while(1):
-        clientConnection, addr = serverSocket.accept();
-        data=clientConnection.recv(1024);
+        client_connection, addr = server_socket.accept();
+        data=client_connection.recv(1024);
         if not data:
             break
         dict = json.loads(data)
-        isValid=False
+        is_valid=False
         if "verb" in dict and "noun" in dict:
-            isValid=returnIsValid(dict)
+            is_valid=return_is_valid(dict)
         else:
-            response=badRequest()
+            response=bad_request()
 
-        if isValid == False:
-            response = badRequest()
+        if is_valid == False:
+            response = bad_request()
         else:
-            response=communicationBus.ExecuteRequest(data.decode("utf-8"))
-        clientConnection.sendall(bytes(response,encoding="utf-8"))
-        clientConnection.close()
-    serverSocket.close()
+            response=communication_bus.execute_request(data.decode("utf-8"))
+        client_connection.sendall(bytes(response,encoding="utf-8"))
+        client_connection.close()
+    server_socket.close()
 
-def returnIsValid(dict):
-    isValid=False
+def return_is_valid(dict):
+    is_valid=False
     for verb in verbs:
         if dict["verb"] == verb:
-            isValid = True
+            is_valid = True
         if dict["verb"] == verbs[1]:
             if "query" not in dict:
-                isValid=False
+                is_valid=False
         elif dict["verb"] == verbs[2]:
             if "query" not in dict or "fields" not in dict:
-                isValid=False
-    return isValid
+                is_valid=False
+    return is_valid
 
-def badRequest():
+def bad_request():
     json_response = {
         "status_code":5000,
         "status":"BAD_FORMAT",
@@ -58,11 +58,11 @@ def badRequest():
 class CommunicationBus:
     def __init__(self,):
         self.JsonXmlAdapter=JsonXmlAdapter()
-    def ExecuteRequest(self, json_obj):
-        xml_obj=self.JsonXmlAdapter.JsonToXml(json_obj)
-        result=self.connectToAdapter(xml_obj)
-        return self.JsonXmlAdapter.XmlToJson(result)
-    def connectToAdapter(self, xml_obj):
+    def execute_request(self, json_obj):
+        xml_obj=self.JsonXmlAdapter.json_to_xml(json_obj)
+        result=self.connect_to_adapter(xml_obj)
+        return self.JsonXmlAdapter.xml_to_json(result)
+    def connect_to_adapter(self, xml_obj):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect(('localhost', 10003))
         s.sendall(bytes(xml_obj, "utf-8"))
@@ -70,5 +70,5 @@ class CommunicationBus:
         s.close()
         return data.decode("utf-8")
 if __name__ == '__main__': 
-    communicationBus= CommunicationBus()
-    openConnection(communicationBus)
+    communication_bus= CommunicationBus()
+    open_connection(communication_bus)
